d3a9434973838e558c13e1969e759df4828208c3
PaulV0001 <valdezpaul00@gmail.com>
2017-08-28 14:57:35 -0500

    Changed all single functions into one that executes them all.
    Removed unneeded comments.

--- js/calculator.js
+++ js/calculator.js
@@ -1,67 +1,30 @@
 "use strict";
 
-function run1() {
-    document.calculator.result.value += "1";
-}
-function run2() {
-    document.calculator.result.value += "2";
-}
-function run3() {
-    document.calculator.result.value += "3";
-}
-function plus() {
-    document.calculator.result.value += "+";
-}
-
-
-function run4() {
-    document.calculator.result.value += "4";
-}
-function run5() {
-    document.calculator.result.value += "5";
-}
-function run6() {
-    document.calculator.result.value += "6";
-}
-function minus() {
-    document.calculator.result.value += "-";
-}
-
-
-
-function run7() {
-    document.calculator.result.value += "7";
-}
-function run8() {
-    document.calculator.result.value += "8";
-}
-function run9() {
-    document.calculator.result.value += "9";
-}
-function multiply() {
-    document.calculator.result.value += "*";
+function btnClick(btn){
+    if (document.calculator.operator.value && document.calculator.left.value !== "") {
+        document.calculator.right.value += btn.getAttribute("value");
+    }else{
+        document.calculator.left.value += btn.getAttribute("value");
+    }
 
 }
 
-
+function operClick(btn) {
+    document.calculator.operator.value = btn.getAttribute("value");
+}
 
 function runClear() {
-    document.calculator.result.value = "";
+    document.calculator.left.value = "";
+    document.calculator.operator.value = "";
+    document.calculator.right.value = "";
 }
-function run0() {
-    document.calculator.result.value += "0";
-}
+
 function total() {
-    var evaluate = eval(document.calculator.result.value);
+    var evaluate = document.calculator.left.value + " " + document.calculator.operator.value + " " + document.calculator.right.value;
+    var total = eval(evaluate);
+    document.calculator.left.value = total;
+    document.calculator.operator.value = "";
+    document.calculator.right.value = "";
+}
 
-    document.calculator.result.value = evaluate;
-}
-function runDivide() {
-    document.calculator.result.value += "/";
-}
-//
-function operate () {
-    var operation = (document.calculator.operator.value)
-    document.calculator.operator = operation
 
-}
