b60423a0afeb5fda2455a3ae19889daa4efd7152
PaulV0001 <valdezpaul00@gmail.com>
2017-08-28 11:19:06 -0500

    Added functions for each number to display in the text field.
    Added an empty string for the clear function.
    
    Added the function for evaluating the numbers.

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,60 @@
-"use strict"
+"use strict";
 
 function run1() {
     document.calculator.result.value += "1";
+}
+function run2() {
+    document.calculator.result.value += "2";
+}
+function run3() {
+    document.calculator.result.value += "3";
+}
+function plus() {
+    document.calculator.result.value += "+";
+}
+
+
+function run4() {
+    document.calculator.result.value += "4";
+}
+function run5() {
+    document.calculator.result.value += "5";
+}
+function run6() {
+    document.calculator.result.value += "6";
+}
+function minus() {
+    document.calculator.result.value += "-";
+}
+
+
+
+function run7() {
+    document.calculator.result.value += "7";
+}
+function run8() {
+    document.calculator.result.value += "8";
+}
+function run9() {
+    document.calculator.result.value += "9";
+}
+function multiply() {
+    document.calculator.result.value += "*";
+}
+
+
+
+function runClear() {
+    document.calculator.result.value = "";
+}
+function run0() {
+    document.calculator.result.value += "0";
+}
+function total() {
+    var evaluate= eval(document.calculator.result.value);
+
+    document.calculator.result.value = evaluate;
+}
+function runDivide() {
+    document.calculator.result.value += "/";
 }
